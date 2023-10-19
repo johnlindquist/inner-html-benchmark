fb31f9456d246df4dfcd417d6e96ae707a558a68
John Lindquist <johnlindquist@gmail.com>
2023-10-19 09:36:55 -0600

    fix(initial): commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  imports: [CommonModule],
+  standalone: true,
+  template: `<div>
+    <button (click)="innerHTMLRowsVisible = !innerHTMLRowsVisible">
+      Toggle innerHTML rows
+    </button>
+    <button (click)="bindingRowsVisible = !bindingRowsVisible">
+      Toggle binding rows
+    </button>
+
+    <div *ngIf="innerHTMLRowsVisible">
+      <h2>innerHTML rows</h2>
+      <div *ngFor="let row of rows" [innerHTML]="row"></div>
+    </div>
+
+    <div *ngIf="bindingRowsVisible">
+      <h2>Binding rows</h2>
+      <div *ngFor="let row of rows">{{ row }}</div>
+    </div>
+  </div> `,
 })
 export class AppComponent {
-  title = 'inner-html-benchmark';
+  innerHTMLRowsVisible = false;
+  bindingRowsVisible = false;
+
+  rows: string[] = Array.from({ length: 10000 }).map((x, i) => i.toString());
 }
